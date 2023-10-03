4cf09b36e01b0ea5eba12218d8ddcd4f55255946
BobJohnsom <kai.barry1234@gmail.com>
2023-10-03 22:00:18 +1000

    more experiments

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-import torch
+corrupted = ['AJBB1c2b',
+'AJPB1c2b',
+'AMMA1c2a',
+'AWGB1c2a',
+'BAOBB1c2a',
+'BCPA1c2a',
+'BDLA1c2a',
+'BFPB1c2a',
+'BFSA1c2a',
+'BGLB1c2b',
+'BRCA1c2a',
+'BRVA1c2b',
+'BSSA1c2b',
+'BCHA2c7a',
+'SSNA1c2a']
+cor = set()
+l = [0,0,0,1]
+for i in l:
+    cor.add(i)
+print(len(cor))
